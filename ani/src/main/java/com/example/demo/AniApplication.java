06f26d81ae92e71a29e744983fbdf1f5375977b7
user <user@DESKTOP-2PI2T2C>
2020-03-28 22:19:24 +0900

    최초 커밋

---
+++ ani/src/main/java/com/example/demo/AniApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AniApplication.class, args);
+	}
+
+}
